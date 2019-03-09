ed50c0468a4567420167c93b0adc47adaad33316
Unknown <Kizary-archer@yandex.ru>
2019-03-09 20:45:31 +0300

    fix

--- gps-gprs_tracker/gps-gprs_tracker.ino
+++ gps-gprs_tracker/gps-gprs_tracker.ino
@@ -1,18 +1,40 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial GPSmodule(2, 3);//RX, TX
-//**********************************************
+SoftwareSerial SIM(2, 3);//RX, TX
+
 void setup()
 {
-GPSmodule.begin(9600);   
-Serial.begin(9600);  
-Serial.print("Please enter AT command:");  
-delay(100);
+  //настройка SIM808 при первом включении
+  SIM.begin(9600);
+  Serial.begin(9600);
+
+  SIM808info();//вывод информации о модуле
+  //Serial.println("Enter command:");
+
 }
 void loop()
 {
- if (Serial.available()>0)
- GPSmodule.write(Serial.read());
- if (GPSmodule.available()>0)
- Serial.write(GPSmodule.read());
+  serialListen();
+  delay(100);
+}
+void serialListen()
+{
+  if (Serial.available() > 0)
+    SIM.write(Serial.read());
+  if (SIM.available() > 0)
+    Serial.write(SIM.read());
+
+}
+void SIM808info()
+{
+  String ATInfo[] = {"port speed:","AT+IPR?","port settings:","AT+ICF?","name","AT+GMM","sim""AT+COPS?"};
+  Serial.println("********SIM808 info***********");
+
+
+  SIM.println("AT+GMM");
+  Serial.write(SIM.read());
+  
+  SIM.println("AT+COPS?");
+  delay(1000);
+  Serial.write(SIM.read());
 }
