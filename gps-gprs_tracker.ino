7108023ee8e32af1ab6d990a82ee39abae26179b
Unknown <Kizary-archer@yandex.ru>
2019-03-14 23:07:35 +0300

    GPS parse

--- gps-gprs_tracker.ino
+++ gps-gprs_tracker.ino
@@ -3,19 +3,20 @@
 SoftwareSerial SIM(2, 3);//RX, TX
 
 #define DEBUG true
+#define phone +7(969)705-57-85
+
 
 
 void(* resetFunc) (void) = 0;//перезагрузка
 
-void setup()
+void setup()  //настройка SIM808 при первом включении
 {
-  //настройка SIM808 при первом включении
   SIM.begin(19200);
   Serial.begin(115200);
-  String Sett[] = {"AT+CFUN=1", "AT+CGNSPWR=1", "AT+CGNSSEQ=GGA"};
+  char *Sett[] = {"AT+CFUN=1", "AT+CGNSPWR=1", "AT+CGNSSEQ=GGA"};
   Serial.println("********SIM808 SETTINGS***********");
-  for (byte i = 0 ; i < 3; i ++) {
-    commandSIM(Sett[i], 10,false, DEBUG);
+  for (byte i = 0 ; i < sizeof(Sett); i ++) {
+    commandSIM(Sett[i], 10, false, DEBUG);
   }
   if (DEBUG)SIM808info(); //вывод информации о модуле
   Serial.println("******************************");
@@ -32,18 +33,46 @@ void loop()
 
 void getGPS()
 {
-  commandSIM("AT+CGNSPWR=1", 10,false, DEBUG);
-  commandSIM("AT+CGNSSEQ=GGA", 10,false, DEBUG);
-  
-  commandSIM("AT+CGPSINF=2", 1000, false, DEBUG);
+  commandSIM("AT+CGNSPWR=1", 10, false, DEBUG);
+  commandSIM("AT+CGNSSEQ=GGA", 10, false, DEBUG);
+  commandSIM("AT+CGPSINF=2", 1000, true, DEBUG);
 }
 
-void parseGPSdata()
+void parseGPSdata(String dataSendGPS)
 {
-  
+
+  Serial.println("-------------");
+  String GPSdata[5] {"","","",""}; //  latitude,longitude,state,satellite
+  int coma[4] = {2, 4, 6};
+  byte CountComa = 0;
+  int i = 0, j = 0, L = dataSendGPS.length();
+  while (i < 90)
+  {
+    if (dataSendGPS[i] == ',')CountComa++;
+
+    i++;
+    if (coma[j] == CountComa) {
+      while (dataSendGPS[i] != ',')
+      {
+        GPSdata[j] += dataSendGPS[i];
+        GPSdata[3] = dataSendGPS[i+2];
+        i++;
+      }
+      j++;
+    }
+  }
+  Serial.println(GPSdata[0]);
+  Serial.println("-------------");
+  Serial.println(GPSdata[1]);
+  Serial.println("-------------");
+  Serial.println(GPSdata[2]);
+  Serial.println("-------------");
+  Serial.println(GPSdata[3]);
+  Serial.println("-------------");
+
 }
 
-void commandSIM(String command, int timeout,boolean GetData, boolean debug) //вывод ответа на AT команду
+void commandSIM(String command, int timeout, boolean GetData, boolean debug) //вывод ответа на AT команду
 {
   String dataSIM808 = "";
   long int t = millis();
@@ -52,7 +81,7 @@ void commandSIM(String command, int timeout,boolean GetData, boolean debug) //в
     if ((t + 10000) < millis())
     {
       Serial.println("Error connect to SIM808...RESET");
-      delay(1000);  
+      delay(1000);
       resetFunc();
     }
 
@@ -68,17 +97,18 @@ void commandSIM(String command, int timeout,boolean GetData, boolean debug) //в
 
 void eventSIM808(String dataSIM808)//события с модуля
 {
-  //Serial.println(dataSIM808);
   int i = 0;
   String event = "";
-  while(dataSIM808[i]!=':')
+  while (dataSIM808[i] != '+') {
+    i++;
+  }
+  i++;
+  while (dataSIM808[i] != '=')
   {
-    while(dataSIM808[i]!='+'){i++;}
     event += dataSIM808[i];
     i++;
-   
-  } 
-  //Serial.println(dataSIM808[2]);  
+  }
+  if (event = "CGPSINF")parseGPSdata(dataSIM808);
 }
 
 void serialListen()//отправка команд в ручном режиме
@@ -95,12 +125,12 @@ void serialListen()//отправка команд в ручном режиме
   }
 }
 
-void SIM808info()//вывод информации о настройках 
+void SIM808info()//вывод информации о настройках
 {
-  char *ATInfo[] = {"name: ", "ATI", "sim: ", "AT+COPS?","phone nomber: ","+7(969)705-57-85", "functionality mode: ", "AT+CFUN?", "GPS power: ", "AT+CGPSPWR?", "GPS mode: ", "AT+CGPSRST?", "GPS parsed mode: ", "AT+CGNSSEQ?"};
+  char *ATInfo[] = {"name: ", "ATI", "sim: ", "AT+COPS?", "functionality mode: ", "AT+CFUN?", "GPS power: ", "AT+CGPSPWR?", "GPS mode: ", "AT+CGPSRST?", "GPS parsed mode: ", "AT+CGNSSEQ?"};
   Serial.println("********SIM808 info***********");
-  for (byte i = 0 ; i < 14; i += 2) {
+  for (byte i = 0 ; i < sizeof(ATInfo) / 2; i += 2) {
     Serial.print(ATInfo[i]);
-    commandSIM(ATInfo[i + 1], 10,false, DEBUG);
+    commandSIM(ATInfo[i + 1], 10, false, DEBUG);
   }
 }
